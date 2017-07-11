6cce71194da2ffddbc8e4a883b5abfbffc509208
Joe Santoiemmo <JSanto333@gmail.com>
2017-07-11 10:46:39 -0400

    Added more to Cat and Program

--- IntroToOOP/Cat.cs
+++ IntroToOOP/Cat.cs
@@ -26,6 +26,11 @@ namespace IntroToOOP
             get { return this.name; }
             set { this.name = value; }
         }
+
+        public int Age
+        {
+            get { return this.age; }
+        }
         //We also have Behaviors.
         //Methods - repeatable/reusable sections of code - Actions
         //Constructors - specialized methods that are used when instantiating an object
@@ -38,6 +43,9 @@ namespace IntroToOOP
             //This is an example of a default constructor. A default constructor takes
             //no arguments/parameters.
         }
+
+        //Can have as many constructors as you need as long as they each have a unique signature
+        //By signature, I mean the parameters in the parentheses after the constructor name.
 
         public Cat(string name, int age, string furColor)
         {
